6728eda96e8e9d760a5f91a06ac23b69c30bfff5
John Tuttle <thejwt@gmail.com>
2017-11-06 21:29:45 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,6 +39,7 @@ class SongsController < Sinatra::Base
       genres << Genre.find_by(name: genre)
     end
     song = Song.update(name: params[:name], artist: artist, genres: genres)
+    binding.pry
     flash[:message] = "Successfully updated song."
     redirect to "/songs/#{song.slug}"
   end
