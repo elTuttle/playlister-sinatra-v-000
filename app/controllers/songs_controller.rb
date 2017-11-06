7f8882cb2736d11735f0b581d711816619550cc9
John Tuttle <thejwt@gmail.com>
2017-11-06 20:03:32 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,11 @@
+class SongsController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/' do
+    erb :index
+  end
+
+
+end
