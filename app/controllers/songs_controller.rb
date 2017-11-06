5c9ada1b1300afca4cb278c5afa52fa82f3f9d80
John Tuttle <thejwt@gmail.com>
2017-11-06 21:15:26 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,7 @@ class SongsController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable :sessions
 
   get '/songs' do
     erb :songs
