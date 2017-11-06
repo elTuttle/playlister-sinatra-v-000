18eb90ad10813f6a3c542f55a832dd2b295f108a
John Tuttle <thejwt@gmail.com>
2017-11-06 19:38:14 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,16 @@ class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
 
+  def slug
+    self.name.split.join("-").downcase
+  end
+
+  def self.find_by_slug(slug_name)
+    real_name = slug_name.split("-")
+    real_name.delete_if {|x| x == "-"}
+    real_name.each {|s| s.capitalize!}
+    real_name = real_name.join(" ")
+    Artist.find_by(name: real_name)
+  end
+
 end
