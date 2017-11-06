c8a49c5e39c83c77b18510a997165eac578779fe
John Tuttle <thejwt@gmail.com>
2017-11-06 19:36:58 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,7 +11,6 @@ class Artist < ActiveRecord::Base
     real_name.delete_if {|x| x == "-"}
     real_name.each {|s| s.capitalize!}
     real_name = real_name.join(" ")
-    binding.pry
     Artist.find_by(name: real_name)
   end
 
